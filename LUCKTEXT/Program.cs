c00b4d6376f1d1858c27dfa12f0293ff172162bb
Carl Halen <carl.halen@elev.ga.ntig.se>
2022-08-31 14:25:46 +0200

    initial commit

---
+++ LUCKTEXT/Program.cs
@@ -0,0 +1,14 @@
+﻿//Console.WriteLine("Hej NTI");
+//Console.WriteLine("Hej TE21B");
+
+//string name;
+
+//name = "Mikcie";
+//name = "Anna";
+
+//Console.WriteLine(name);
+
+//Console.ReadLine();
+
+//Console.WriteLine("Vad heter du");
+
